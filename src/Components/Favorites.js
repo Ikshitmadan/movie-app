08850fa716c34cc76f6a3b192e0dee0de385fee6
ikshitmadan <ikshitmadan71@gmail.com>
2022-07-25 15:41:07 +0530

    :construction: genre rendering

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,20 +1,102 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import { API_KEY } from '../secret';
+
 
 export class Favorites extends Component {
+constructor(){
+
+  super()
+this.state={
+  movies:[],
+  genre:[]
+}
+  
+}
+async componentDidMount() {
+  let ans=await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+
+  let genreId = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Sci-Fi",
+    10770: "TV",
+    53: "Thriller",
+    10752: "War",
+    37: "Western",
+  };
+ 
+  let genreArr = [];
+  ans.data.results.map((movieObj) => {
+    if (!genreArr.includes(genreId[movieObj.genre_ids[0]])) {
+      genreArr.push(genreId[movieObj.genre_ids[0]]);
+    }
+  });
+this.setState({
+movies: [...ans.data.results], 
+genre:[...genreArr]
+});
+}
+
+
+
   render() {
+    // let movie=movies.results;
+
+    let genreId = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
     return (
       <div>
 
 <div className="container text-center">
   <div className="row">
     <div className="col-3" >
-    <ul className="list-group">
-  <li className="list-group-item active" aria-current="true">An active item</li>
-  <li className="list-group-item">All Generes</li>
-  <li className="list-group-item">Fantasy</li>
+    <ul className="list-group fav-list">
+  <li className="list-group-item active" aria-current="true">All Generes</li>
+
+{
+this.state.genre.map((genre)=>(
+ <li className="list-group-item">{genre}</li>
+))
+
+}
+
+
+  {/* <li className="list-group-item">Fantasy</li>
   <li className="list-group-item">Action</li>
   <li className="list-group-item">Horror</li>
-  
+   */}
 </ul>
     </div>
    
@@ -24,28 +106,36 @@ export class Favorites extends Component {
 <input type="text"  className='col' placeholder='5'/>
  </div>
  <div className='row'>
- <table class="table">
+ <table className="table fav-table">
   <thead>
     <tr>
       <th scope="col">Title</th>
       <th scope="col">Genre</th>
       <th scope="col">Popularity</th>
       <th scope="col">Rating</th>
+      <th scope="col"></th>
+
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
+  
+      {
+        this.state.movies.map((movieObj)=>(
+          <tr>
+          <td scope='row' style={{width:"10rem"}}><img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} className="card-img-top movie-image" alt="..."  />
+ {movieObj.original_title}</td>
+          <td>{genreId[movieObj.genre_ids[0]] }</td>
+          <td>{movieObj.popularity}</td>
+          <td>{movieObj.vote_average}</td>
+          <td>
+          <button type="button" className="btn btn-outline-danger">Remove</button>
+          </td>
+          </tr>
+        )
+        )
+      }
+   
+  
     
   </tbody>
 </table>
